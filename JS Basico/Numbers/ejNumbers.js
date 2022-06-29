b6e93c24656e2fa7a2985791821ef94297b6d06c
Javier Curto Brull <curto.brull.javier@gmail.com>
2022-06-29 21:23:28 +0200

    Ejercicio Numbers

---
+++ JS Basico/Numbers/ejNumbers.js
@@ -0,0 +1,17 @@
+var heightInt = 186;
+
+var heightFloat = 1.86;
+
+var weightFloat = 98.5;
+
+var heightCeil = Math.ceil(heightFloat);
+
+var weightFloor = Math.floor(weightFloat);
+
+var topNum = Number.MAX_VALUE;
+
+var topNumPlusOne = Number.MAX_VALUE + 1;
+
+var num = topNum === topNumPlusOne;
+
+console.log(num);
